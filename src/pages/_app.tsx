856abc19a4b4cfb83037c011e8cd55d54757bcc4
wuaicot <wuaicot@gmail.com>
2025-04-22 17:39:39 -0400

    implementing resister

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+// client/src/pages/_app.tsx
+import '@/styles/globals.css'; // Asegúrate que tus estilos globales estén importados
+import type { AppProps } from 'next/app';
+import { Provider } from 'react-redux';
+import { store } from '../store'; // Importa tu store
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}> {/* Envuelve la app con el Provider */}
+      <Component {...pageProps} />
+    </Provider>
+  );
 }

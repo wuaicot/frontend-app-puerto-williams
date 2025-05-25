0e59d162cf7bb21ca52f7fed4b7572532509b215
wuaicot <wuaicot8@gmail.com>
2025-05-24 23:48:12 -0400

    add SpeedInsights

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,13 @@ import '@/styles/globals.css'; // Asegúrate que tus estilos globales estén imp
 import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
 import { store } from '../store'; // Importa tu store
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}> {/* Envuelve la app con el Provider */}
+      {/* Speed Insights para métricas de rendimiento */}
+      <SpeedInsights />
       <Component {...pageProps} />
     </Provider>
   );

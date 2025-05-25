8cda2912af053546ab4f02d994c75cf2b6e24286
wuaicot <wuaicot8@gmail.com>
2025-05-25 01:55:25 -0400

    add Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,12 +4,14 @@ import type { AppProps } from 'next/app';
 import { Provider } from 'react-redux';
 import { store } from '../store'; // Importa tu store
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from "@vercel/analytics/next"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}> {/* Envuelve la app con el Provider */}
       {/* Speed Insights para métricas de rendimiento */}
       <SpeedInsights />
+      <Analytics />
       <Component {...pageProps} />
     </Provider>
   );

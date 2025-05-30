c5fd1d8f5ac18145a6654b466f616c427910b3f2
wuaicot <wuaicot8@gmail.com>
2025-05-30 18:01:46 -0400

    tunning mantenimiento

--- src/components/BotoneraMainMantenimiento.tsx
+++ src/components/BotoneraMainMantenimiento.tsx
@@ -1,46 +1,36 @@
 // client/src/components/BotoneraMainMantenimiento.tsx
-
 import React from "react";
 import { useRouter } from "next/router";
-import Link from "next/link";
 
 export const BotoneraMainMantenimiento: React.FC = () => {
   const router = useRouter();
-  const botones = [
-    { label: "Registrar inicio/fin turno", route: "/conserjeria/turno" },
-    { label: "Tareas asignadas", route: "/conserjeria/tareas" },
+  const botones = [   
+    { label: "Ver registros", route: "/conserjeria/registros" },     
     { label: "Control de pisos", route: "/conserjeria/pisos" },
     { label: "Control de muros", route: "/conserjeria/muros" },
-    { label: "Ver registros", route: "/conserjeria/registros" },
+    { label: "Tareas asignadas", route: "/conserjeria/tareas" },    
+    { label: "Registrar inicio/fin turno", route: "/conserjeria/turno" },
   ];
 
   return (
-    <div className="relative flex flex-col items-center gap-6 mt-6 text-white">
-      <h1 className="text-xl sm:text-2xl font-semibold text-center mb-4 md:mb-6 lg:mb-8 text-slate-300">
-        Panel de control Mantenimiento
+    <div className="relative flex flex-col items-center gap-6 mt-6 text-white bg-gray-800 p-6 rounded-2xl shadow-lg">
+      <h1 className="text-xl sm:text-2xl font-semibold text-center text-slate-300">
+        Panel de Mantenimiento
       </h1>
-
-      <div className="flex flex-col gap-3 w-full max-w-sm p-4 rounded-2xl
-        md:flex-row md:flex-wrap md:justify-center md:gap-4 lg:gap-6"
-      >
+      <div className="flex flex-col gap-4 w-full max-w-sm
+                      md:flex-row md:flex-wrap md:justify-center md:gap-6">
         {botones.map((btn) => (
           <button
             key={btn.route}
             onClick={() => router.push(btn.route)}
-            className="w-full md:w-auto py-2 px-4 rounded-full border border-sky-500 text-center hover:bg-white hover:text-black transition
-              duration-300 ease-in-out shadow-lg bg-sky-500 text-white font-semibold text-sm md:text-base transform hover:scale-105"
+            className="w-full md:w-auto py-2 px-4 rounded-full border border-sky-500
+                       bg-sky-500 text-white font-semibold text-sm md:text-base
+                       hover:bg-white hover:text-black transition transform hover:scale-105"
           >
             {btn.label}
           </button>
         ))}
       </div>
-
-      <Link
-        href="/"
-        className="absolute bottom-4 left-4 text-xl hover:opacity-70 cursor-pointer animate-pulse md:bottom-6 md:left-6 lg:text-2xl"
-      >
-        ←
-      </Link>
     </div>
   );
 };
